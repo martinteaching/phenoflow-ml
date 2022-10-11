f058a4a85130432e316dbf58bde09290aaab9188
Antonio López Martínez-Carrasco <anlopezmc@gmail.com>
2022-10-11 14:52:01 +0100

    Template of the step3 modified and tested (it works).

--- web/templates/tbc/step3.py
+++ web/templates/tbc/step3.py
@@ -19,11 +19,12 @@ with open(sys.argv[1], 'r') as file_in, open('matrix_of_matches.json', 'w') as f
     for cluster_number_of_partition_k in range(0, k):
         dictionary_matrix_of_matches["partition_k_"+str(k)+"_cluster_"+str(cluster_number_of_partition_k)] = dict()
         dictionary_matrix_of_matches["partition_k_"+str(k)+"_cluster_"+str(cluster_number_of_partition_k)]["instances"] = partitions["partition_k_"+str(k)]["cluster_"+str(cluster_number_of_partition_k)]
+        dictionary_matrix_of_matches["partition_k_"+str(k)+"_cluster_"+str(cluster_number_of_partition_k)]["match_with_previous_partitions"] = dict()
         # Python set with the indexes of the instances from the original dataset.
         cluster_of_partition_k = set( partitions["partition_k_"+str(k)]["cluster_"+str(cluster_number_of_partition_k)] )
         # Iterate over the previous partitions (from the partition 2 to the partition k-1).
         for partition_number in range(2, k):
-            dictionary_matrix_of_matches["partition_k_"+str(k)+"_cluster_"+str(cluster_number_of_partition_k)]["partition_k_"+str(partition_number)] = dict()
+            dictionary_matrix_of_matches["partition_k_"+str(k)+"_cluster_"+str(cluster_number_of_partition_k)]["match_with_previous_partitions"]["partition_k_"+str(partition_number)] = dict()
             max_value_of_match = float("-inf")
             cluster_with_max_value_of_match = None
             # Iterate over the clusters of the current partition.
@@ -33,7 +34,7 @@ with open(sys.argv[1], 'r') as file_in, open('matrix_of_matches.json', 'w') as f
                 if value_of_match > max_value_of_match:
                     max_value_of_match = value_of_match
                     cluster_with_max_value_of_match = "partition_k_"+str(partition_number)+"_cluster_"+str(cluster_number)
-            dictionary_matrix_of_matches["partition_k_"+str(k)+"_cluster_"+str(cluster_number_of_partition_k)]["partition_k_"+str(partition_number)]["max_value_of_match"] = max_value_of_match
-            dictionary_matrix_of_matches["partition_k_"+str(k)+"_cluster_"+str(cluster_number_of_partition_k)]["partition_k_"+str(partition_number)]["cluster_with_max_value_of_match"] = cluster_with_max_value_of_match
+            dictionary_matrix_of_matches["partition_k_"+str(k)+"_cluster_"+str(cluster_number_of_partition_k)]["match_with_previous_partitions"]["partition_k_"+str(partition_number)]["max_value_of_match"] = max_value_of_match
+            dictionary_matrix_of_matches["partition_k_"+str(k)+"_cluster_"+str(cluster_number_of_partition_k)]["match_with_previous_partitions"]["partition_k_"+str(partition_number)]["cluster_with_max_value_of_match"] = cluster_with_max_value_of_match
     # Finally, we convert all to json format and write to the output file.
     json.dump(dictionary_matrix_of_matches, file_out, indent=4)
