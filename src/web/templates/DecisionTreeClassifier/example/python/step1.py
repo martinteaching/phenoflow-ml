c68d7919d7ace54c84dffa8bc201f74ebfc404e7
Antonio López Martínez-Carrasco <anlopezmc@gmail.com>
2025-02-16 22:24:15 +0100

    Docs improved. Templates for 'DecisionTreeClassifier' added (not tested).

---
+++ src/web/templates/DecisionTreeClassifier/example/python/step1.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+# Author:
+#    Antonio Lopez-Martinez-Carrasco <antoniolopezmc@um.es>
+
+import sys
+from pandas import read_csv
+
+train_dataset = read_csv(sys.argv[1])
+train_dataset.to_csv('name_example_id_1_train_dataset.csv', index = False)
+test_dataset = read_csv(sys.argv[2])
+test_dataset.to_csv('name_example_id_1_test_dataset.csv', index = False)
